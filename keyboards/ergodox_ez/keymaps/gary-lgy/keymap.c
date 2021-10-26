cd9b74b0b502483c7c08a76479425a3830aa0a11
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2021-10-26 17:07:05 +0800

    Remove NAV layer

--- keyboards/ergodox_ez/keymaps/gary-lgy/keymap.c
+++ keyboards/ergodox_ez/keymaps/gary-lgy/keymap.c
@@ -4,7 +4,6 @@
 
 enum layers {
     LAYER_BLOCK,
-    LAYER_NAV,
     LAYER_SYM,
     LAYER_NUM,
     LAYER_FUNC,
@@ -42,44 +41,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_ESCAPE,    C(KC_B),       KC_MS_BTN3,
 
             // right hand
-            KC_CAPSLOCK,    KC_SLASH,    KC_K,                      KC_L,             KC_C,       KC_R,       KC_F,
-            KC_TAB,         KC_MINUS,    KC_M,                      KC_H,             KC_T,       KC_S,       KC_B,
-            /*none*/        KC_EQUAL,    KC_V,                      KC_N,             KC_D,       KC_G,       KC_X,
-            KC_NO,          KC_RCTRL,    KC_RGUI,                   KC_RSHIFT,        KC_RALT,    KC_UP,      KC_GRAVE,
-            /*none*/        /*none*/     LT(LAYER_NAV,KC_SPACE),    TT(LAYER_SYM),    KC_LEFT,    KC_DOWN,    KC_RIGHT,
+            KC_CAPSLOCK,    KC_SLASH,    KC_K,        KC_L,             KC_C,       KC_R,       KC_F,
+            KC_TAB,         KC_MINUS,    KC_M,        KC_H,             KC_T,       KC_S,       KC_B,
+            /*none*/        KC_EQUAL,    KC_V,        KC_N,             KC_D,       KC_G,       KC_X,
+            KC_NO,          KC_RCTRL,    KC_RGUI,     KC_RSHIFT,        KC_RALT,    KC_UP,      KC_GRAVE,
+            /*none*/        /*none*/     KC_SPACE,    TT(LAYER_SYM),    KC_LEFT,    KC_DOWN,    KC_RIGHT,
 
             // right thumb
             C(KC_LEFT),    C(KC_RIGHT),
             KC_UP,
             KC_DOWN,       A(KC_SPACE),    KC_ENTER
-
-            ),
-
-[LAYER_NAV] = LAYOUT_ergodox(
-
-        // left hand
-        KC_NO,      KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-        KC_NO,      KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
-        KC_NO,      KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,
-        KC_NO,      KC_NO,    _______,    _______,    _______,    _______,    KC_NO,
-        _______,    KC_NO,    KC_NO,      KC_NO,      _______,
-
-        // left thumb
-        /*none*/    KC_NO,      KC_NO,
-        /*none*/    /*none*/    KC_NO,
-        KC_NO,      KC_NO,      KC_NO,
-
-        // right hand
-        KC_NO,      KC_NO,      KC_NO,      KC_HOME,    KC_UP,      KC_END,       KC_NO,
-        KC_NO,      KC_NO,      KC_NO,      KC_LEFT,    KC_DOWN,    KC_RIGHT,     KC_NO,
-        /*none*/    KC_NO,      KC_NO,      KC_PGUP,    KC_TAB,     KC_PGDOWN,    KC_NO,
-        KC_NO,      _______,    _______,    _______,    _______,    KC_NO,        KC_NO,
-        /*none*/    /*none*/    KC_NO,      KC_NO,      KC_NO,      KC_NO,        KC_NO,
-
-        // right thumb
-        KC_NO,    KC_NO,
-        KC_NO,
-        KC_NO,    KC_NO,    KC_NO
 
             ),
 
@@ -126,11 +97,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_NO,      KC_NO,      KC_NO,
 
           // right hand
-          KC_NO,      KC_NO,      KC_COMMA,    KC_7,       KC_8,       KC_9,     KC_NO,
-          KC_NO,      KC_NO,      KC_DOT,      KC_4,       KC_5,       KC_6,     KC_NO,
-          /*none*/    KC_NO,      KC_0,        KC_1,       KC_2,       KC_3,     KC_NO,
-          KC_NO,      _______,    _______,     _______,    _______,    KC_NO,    KC_NO,
-          /*none*/    /*none*/    _______,     KC_NO,      KC_NO,      KC_NO,    KC_NO,
+          KC_NO,      KC_NO,      KC_COMMA,    KC_7,       KC_8,       KC_9,         KC_NO,
+          KC_NO,      KC_NO,      KC_DOT,      KC_4,       KC_5,       KC_6,         KC_NO,
+          /*none*/    KC_NO,      KC_0,        KC_1,       KC_2,       KC_3,         KC_NO,
+          KC_NO,      _______,    _______,     _______,    _______,    KC_PGUP,      KC_NO,
+          /*none*/    /*none*/    _______,     KC_NO,      KC_HOME,    KC_PGDOWN,    KC_END,
 
           // right thumb
           KC_NO,    KC_NO,
