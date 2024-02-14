4fbc4f8c59056db0c0fa6fbbbe1fbd809571fbc4
Liu Guangyuan (Gary) <garyliu2122@gmail.com>
2024-02-14 10:41:57 +0000

    more comfortable gaming layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/gary-lgy/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/gary-lgy/keymap.c
@@ -49,14 +49,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_GAMING] = LAYOUT_5x6(
 
-          KC_GRAVE,             KC_1,    KC_2,       KC_3,       KC_4,    KC_5,         /*-*/       /*---*/    /*-*/       KC_6,                          KC_7,    KC_8,        KC_9,                                            KC_0,       KC_MINUS,        \
-          KC_TAB,               KC_Q,    KC_W,       KC_E,       KC_R,    KC_T,         /*-*/       /*---*/    /*-*/       KC_Y,                          KC_U,    KC_I,        KC_O,                                            KC_P,       KC_EQUAL,        \
-          KC_CAPS_LOCK,         KC_A,    KC_S,       KC_D,       KC_F,    KC_G,         /*-*/       /*---*/    /*-*/       KC_H,                          KC_J,    KC_K,        KC_L,                                            KC_SCLN,    KC_QUOTE,        \
-          MO(LAYER_CONTROL),    KC_Z,    KC_X,       KC_C,       KC_V,    KC_B,         /*-*/       /*---*/    /*-*/       KC_N,                          KC_M,    KC_COMMA,    KC_DOT,                                          KC_SLSH,    KC_BACKSLASH,    \
-          /*-*/                 /*-*/    KC_LGUI,    KC_LALT,    /*-*/    /*-*/         /*-*/       /*---*/    /*-*/       /*-*/                          /*-*/    KC_LBRC,     KC_RBRC,                                         /*-*/       /*-*/            \
-          /*-*/                 /*-*/    /*-*/       /*-*/       /*-*/    KC_SPACE,     KC_LCTL,    /*---*/    KC_NO,      LT(LAYER_NAV,KC_BACKSPACE),    /*-*/    /*-*/        /*-*/                 /*-*/       /*-*/            \
-          /*-*/                 /*-*/    /*-*/       /*-*/       /*-*/    KC_LSFT,      KC_TAB,     /*---*/    KC_NO,      KC_ENTER,                      /*-*/    /*-*/        /*-*/    /*-*/       /*-*/            \
-          /*-*/                 /*-*/    /*-*/       /*-*/       /*-*/    KC_ESCAPE,    KC_NO,      /*---*/    KC_RALT,    KC_RSFT                        /*-*/    /*-*/        /*-*/    /*-*/       /*-*/            \
+          KC_GRAVE,             KC_1,    KC_2,       KC_3,       KC_4,    KC_5,         /*-*/       /*---*/    /*-*/       KC_6,                          KC_7,    KC_8,        KC_9,       KC_0,       KC_MINUS,        \
+          KC_TAB,               KC_T,    KC_Q,       KC_W,       KC_E,    KC_R,         /*-*/       /*---*/    /*-*/       KC_Y,                          KC_U,    KC_I,        KC_O,       KC_P,       KC_EQUAL,        \
+          KC_CAPS_LOCK,         KC_G,    KC_A,       KC_S,       KC_D,    KC_F,         /*-*/       /*---*/    /*-*/       KC_H,                          KC_J,    KC_K,        KC_L,       KC_SCLN,    KC_QUOTE,        \
+          MO(LAYER_CONTROL),    KC_B,    KC_Z,       KC_X,       KC_C,    KC_V,         /*-*/       /*---*/    /*-*/       KC_N,                          KC_M,    KC_COMMA,    KC_DOT,     KC_SLSH,    KC_BACKSLASH,    \
+          /*-*/                 /*-*/    KC_LGUI,    KC_LALT,    /*-*/    /*-*/         /*-*/       /*---*/    /*-*/       /*-*/                          /*-*/    KC_LBRC,     KC_RBRC,    /*-*/       /*-*/            \
+          /*-*/                 /*-*/    /*-*/       /*-*/       /*-*/    KC_SPACE,     KC_LCTL,    /*---*/    KC_NO,      LT(LAYER_NAV,KC_BACKSPACE),    /*-*/    /*-*/        /*-*/       /*-*/       /*-*/            \
+          /*-*/                 /*-*/    /*-*/       /*-*/       /*-*/    KC_LSFT,      KC_TAB,     /*---*/    KC_NO,      KC_ENTER,                      /*-*/    /*-*/        /*-*/       /*-*/       /*-*/            \
+          /*-*/                 /*-*/    /*-*/       /*-*/       /*-*/    KC_ESCAPE,    KC_NO,      /*---*/    KC_RALT,    KC_RSFT                        /*-*/    /*-*/        /*-*/       /*-*/       /*-*/            \
 
    ),
 
